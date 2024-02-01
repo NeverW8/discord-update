9b45051e744d2b0141f46981ecd5cc36ff5f7504
carl.skantz <carl.skantz@trell.se>
2024-02-01 16:02:06 +0100

    Inital commit, added some basic readme information and the discord script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# discord-update
+# Discord-update
 A simple bash script to Update discord on Debian based systems.
+
+## How to run
+In your terminal, run these two commands:
+Chmod +x to make it executable
+`sudo chmod +x discord-update.sh`
+Then run it like this:
+`./discord-update.sh`
+
+Shouldn't be anything more than that.
+
+I made this for a friend, so if you wanna suggest changes or bugs, open an issue. Have a nice day. :)
---
+++ discord-update.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+discord_link=$(curl -s https://discord.com/api/download\?platform\=linux\&format\=deb | grep -oP '<a href="\K[^"]+' | awk 'NR==1')
+version=$(echo "$discord_link" | grep -oP 'linux/([^/]+)/discord' | awk -F'/' '{print $2}')
+
+
+if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
+    echo "Your OS supports .deb packages, continuing..."
+else
+    echo "Your OS does not support .deb packages"
+    exit 1
+fi
+
+if [ "$(dpkg -l | grep discord)" ]; then
+    echo "Discord is already installed, checking version..."
+    current_version=$(dpkg -l | grep discord | awk '{print $3}')
+    if [ "$current_version" == "$version" ]; then
+        echo "Discord is up to date, exiting..."
+        echo "Current version: $current_version"
+        echo "Latest version: $version"
+        exit 0
+    else
+        echo "Discord is not up to date, updating..."
+        echo "Current version: $current_version"
+        echo "Latest version: $version"
+    fi
+else
+    echo "Discord is not installed, installing..."
+fi
+
+
+wget -O discord.deb "$discord_link"
+
+echo "Installing Discord $version..."
+echo "Requires sudo access to install, please enter your password when prompted"
+sudo dpkg -i discord.deb
+
+echo "Do you want to delete the .deb file? [y/n]"
+read -r delete
+if [ "$delete" == "y" ]; then
+    echo "Deleting .deb file..."
+    rm discord.deb
+else
+    echo "Not deleting .deb file..."
+fi
