395bb3030c3421216ff38bbaf26722180d6a8776
carl.skantz <carl.skantz@trell.se>
2024-02-13 15:03:45 +0100

    Wrapped the code into functions

--- discord-update.sh
+++ discord-update.sh
@@ -3,37 +3,47 @@
 discord_link=$(curl -s https://discord.com/api/download\?platform\=linux\&format\=deb | grep -oP '<a href="\K[^"]+' | awk 'NR==1')
 version=$(echo "$discord_link" | grep -oP 'linux/([^/]+)/discord' | awk -F'/' '{print $2}')
 
-
-if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
-    echo "Your OS supports .deb packages, continuing..."
-else
-    echo "Your OS does not support .deb packages"
-    exit 1
-fi
-
-if [ "$(dpkg -l | grep discord)" ]; then
-    echo "Discord is already installed, checking version..."
-    current_version=$(dpkg -l | grep discord | awk '{print $3}')
-    if [ "$current_version" == "$version" ]; then
-        echo "Discord is up to date, exiting..."
-        echo "Current version: $current_version"
-        echo "Latest version: $version"
-        exit 0
+function os_check() {
+    if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
+        echo "Your OS supports .deb packages, continuing..."
     else
-        echo "Discord is not up to date, updating..."
-        echo "Current version: $current_version"
-        echo "Latest version: $version"
+        echo "Your OS does not support .deb packages"
+        exit 1
     fi
-else
-    echo "Discord is not installed, installing..."
-fi
+}
 
+function discord_check() {
+    if [ "$(dpkg -l | grep discord)" ]; then
+        echo "Discord is already installed, checking version..."
+        current_version=$(dpkg -l | grep discord | awk '{print $3}')
+        if [ "$current_version" == "$version" ]; then
+            echo "Discord is up to date, exiting..."
+            echo "Current version: $current_version"
+            echo "Latest version: $version"
+            exit 0
+        else
+            echo "Discord is not up to date, updating..."
+            echo "Current version: $current_version"
+            echo "Latest version: $version"
+        fi
+    else
+        echo "Discord is not installed, installing..."
+    fi
+}
 
-wget -O discord.deb "$discord_link"
+function discord_install() {
+    wget -O discord.deb "$discord_link"
 
-echo "Installing Discord $version..."
-echo "Requires sudo access to install, please enter your password when prompted"
-sudo dpkg -i discord.deb
+    echo "Installing Discord $version..."
+    echo "Requires sudo access to install, please enter your password when prompted"
+    sudo dpkg -i discord.deb
+}
+
+os_check
+discord_check
+discord_install
+
+echo "Done"
 
 echo "Do you want to delete the .deb file? [y/n]"
 read -r delete
